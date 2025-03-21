a3cbd146362b6b59f3ec8de92a2a393e46e95308
Kristinkus <kristy2202lovely@gmail.com>
2025-03-21 10:38:55 +0300

    Update main.cpp
    Объявление массива структур в мэине

--- Lab3/main.cpp
+++ Lab3/main.cpp
@@ -4,26 +4,31 @@
 #include <windows.h>
 void multiplicity_four(int number);
 
-int main() {
-	SetConsoleOutputCP(CP_UTF8);   кодировка консоли в UTF-8
-	SetConsoleCP(CP_UTF8);         кодировка ввода в UTF-8
+int main() {     
 	setlocale(LC_ALL, "ru");
 	int number;
-	int n;
-
-	int task = inputIntInRange("Введите номер задания: ", 1, 2);
-
-	switch(task)
+	int n=0;
+	Student* unique = (Student*)malloc(n * sizeof(Student));
+	while (true)
 	{
-	case 1:
-		number = inputInt("Введите целое число: ");
-		multiplicity_four(number);
-		break;
-	case 2:
-		system("cls");
-		task2();
-		break;
-	}
+		int task = inputIntInRange("Введите номер задания: ", 1, 2);
 
+		switch (task)
+		{
+		case 1:
+			number = inputInt("Введите целое число: ");
+			multiplicity_four(number);
+			break;
+		case 2:
+			system("cls");
+			task2(unique);
+			break;
+		}
+		int a = inputIntInRange("\nЖелаете продолжить(1-да, 2 - нет): ", 1, 2);
+
+		if (continue_prog(a)) break;
+		else system("cls");
+	}
+	
 	return 0;
 }
